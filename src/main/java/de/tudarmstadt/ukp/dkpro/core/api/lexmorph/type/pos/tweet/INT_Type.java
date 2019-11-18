55df525e73a41cfba9dd5c1aea84b0ab32d86d54
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2019-11-18 17:38:41 +0100

    Reset to Waheds 1.7.5

---
+++ src/main/java/de/tudarmstadt/ukp/dkpro/core/api/lexmorph/type/pos/tweet/INT_Type.java
@@ -0,0 +1,39 @@
+
+/* First created by JCasGen Mon Nov 18 17:36:39 CET 2019 */
+package de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.tweet;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.O_Type;
+
+/** proper noun + verbal
+ * Updated by JCasGen Mon Nov 18 17:36:39 CET 2019
+ * @generated */
+public class INT_Type extends O_Type {
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = INT.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.tweet.INT");
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public INT_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+  }
+}
+
+
+
+    
